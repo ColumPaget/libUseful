8ae025d7e6dc2e6bdfab77eb67b0f54bcc692af6
Colum Paget <colums.projects@gmail.com>
2018-03-04 16:45:02 +0000

    Fix Spawn again, this time for real

--- SpawnPrograms.c
+++ SpawnPrograms.c
@@ -5,6 +5,7 @@
 #include "Stream.h"
 #include "String.h"
 #include "Errors.h"
+#include "FileSystem.h"
 #include <sys/ioctl.h>
 
 
@@ -33,7 +34,7 @@ char *MakeShellSafeString(char *RetStr, const char *String, int SafeLevel)
 int BASIC_FUNC_EXEC_COMMAND(void *Command, int Flags)
 {
     int result;
-    char *Token=NULL, *FinalCommand=NULL;
+    char *Token=NULL, *FinalCommand=NULL, *ExecPath=NULL;
     char **argv;
     const char *ptr;
     int i;
@@ -41,33 +42,36 @@ int BASIC_FUNC_EXEC_COMMAND(void *Command, int Flags)
     if (Flags & SPAWN_TRUST_COMMAND) FinalCommand=CopyStr(FinalCommand, (char *) Command);
     else FinalCommand=MakeShellSafeString(FinalCommand, (char *) Command, 0);
 
+    StripTrailingWhitespace(FinalCommand);
     if (Flags & SPAWN_NOSHELL)
     {
         argv=(char **) calloc(101,sizeof(char *));
         ptr=FinalCommand;
         ptr=GetToken(FinalCommand,"\\S",&Token,GETTOKEN_QUOTES);
-        argv[0]=CopyStr(argv[0],Token);
+        ExecPath=FindFileInPath(ExecPath,Token,getenv("PATH"));
         i=0;
 
         if (! (Flags & SPAWN_ARG0))
         {
-            argv[1]=CopyStr(argv[1],Token);
+            argv[0]=CopyStr(argv[0],ExecPath);
             i=1;
         }
 
         for (; i < 100; i++)
         {
-            if (! ptr) break;
             ptr=GetToken(ptr,"\\S",&Token,GETTOKEN_QUOTES);
+            if (! ptr) break;
             argv[i]=CopyStr(argv[i],Token);
         }
-        execv(argv[0],argv);
+
+        execv(ExecPath,argv);
     }
     else result=execl("/bin/sh","/bin/sh","-c",(char *) Command,NULL);
 
     RaiseError(ERRFLAG_ERRNO, "Spawn", "Failed to execute '%s'",Command);
 //We'll never get to here unless something fails!
     DestroyString(FinalCommand);
+    DestroyString(ExecPath);
     DestroyString(Token);
 
     return(result);
