03d3cee4eefbe1bb2b8c274b01f92348f978cadd
colum <colums.projects@gmail.com>
2023-07-27 19:36:20 +0100

    parse super- keymodifier

--- TerminalKeys.c
+++ TerminalKeys.c
@@ -574,16 +574,22 @@ int TerminalTranslateKeyStrWithMod(const char *str, int *mod)
             if (mod !=NULL) *mod |= KEYMOD_ALT;
             str+=4;
         }
-        else if ((strncasecmp(str, "lalt-", 4)==0) || (strncasecmp(str, "lalt_", 4)==0))
+        else if ((strncasecmp(str, "lalt-", 5)==0) || (strncasecmp(str, "lalt_", 5)==0))
         {
             if (mod !=NULL) *mod |= KEYMOD_ALT;
             str+=5;
         }
-        else if ((strncasecmp(str, "ralt-", 4)==0) || (strncasecmp(str, "ralt_", 4)==0))
+        else if ((strncasecmp(str, "ralt-", 5)==0) || (strncasecmp(str, "ralt_", 5)==0))
         {
             if (mod !=NULL) *mod |= KEYMOD_ALT2;
             str+=5;
         }
+        else if ((strncasecmp(str, "super-", 6)==0) || (strncasecmp(str, "super_", 6)==0))
+        {
+            if (mod !=NULL) *mod |= KEYMOD_SUPER;
+            str+=6;
+        }
+
         else break;
     }
 
